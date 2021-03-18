7955adcc54b5e51f1d266f4e93e7c78c7f5a50b5
Everton95 <79066706+Everton95@users.noreply.github.com>
2021-03-17 22:28:04 -0300

    Add files via upload

---
+++ Atividade5/operacoes.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    function somarValores(){
+        var s1 = parseInt(document.getElementById("txt1").value);
+        var s2 = parseInt(document.getElementById("txt2").value);
+        var s3 = s1+s2;
+        
+		document.getElementById('s3').innerHTML = s3;
+			return s3;
+		
+    }
+</script>
